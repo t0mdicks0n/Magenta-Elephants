c3c8e9dd505f815b727ef837dac0e7d486f1e9d0
oriooctopus <oliverullman@gmail.com>
2017-05-03 18:55:21 -0700

    front end first draft and basic server routing

--- server/index.js
+++ server/index.js
@@ -2,10 +2,6 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/../client'));
-console.log(__dirname + '/../client');
-// app.get('/', function(req, res) {
-//   res.send('HELLO!!!');
-// });
 
 app.listen(3000, function() {
   console.log('Listening on port 3000');
