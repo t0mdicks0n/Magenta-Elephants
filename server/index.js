b3cc03cb617e4744ae24f79f89f4ea8008988758
Hack Reactor Student <student@hackreactor.com>
2017-05-03 22:16:05 -0700

    "server routing template started"

--- server/index.js
+++ server/index.js
@@ -6,3 +6,19 @@ app.use(express.static(__dirname + '/../client'));
 app.listen(3000, function() {
   console.log('Listening on port 3000');
 });
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+});
+
+app.post('/', function (req, res) {
+  res.send('Got a POST request')
+});
+
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user')
+});
+
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user')
+});
