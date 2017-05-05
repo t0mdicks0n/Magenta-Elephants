37d34ca5c468b7d4b9ca111f788d152ae64369a6
oriooctopus <oliverullman@gmail.com>
2017-05-04 19:45:23 -0700

    user can now update a question

--- client/src/components/AnswerQuestion.jsx
+++ client/src/components/AnswerQuestion.jsx
@@ -1,11 +1,52 @@
 import React from 'react';
+import $ from 'jquery';
 
-const AnswerQuestion = props => (
-  <form>  
-    <h1 className="headline">Your Answer Here</h1>
-    <textarea placeholder="Answer Question Here"></textarea>
-    <button>Submit</button>
-  </form>
-);
+class AnswerQuestion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      answerBody: ''
+    }
+    this.updateQuestion = this.updateQuestion.bind(this);
+    this.updateAnswer = this.updateAnswer.bind(this);
+  }
+
+  updateQuestion(e) {
+    e.preventDefault();
+    var obj = {
+     questionId: 5, 
+     expertId: 1,
+     answer: this.state.answerBody
+    };
+
+    $.ajax({
+      type: 'PUT',
+      url: '/questions',
+      data: obj,
+      success: (data) => {
+        console.log('success!', data);
+      },
+      error: (err) => {
+        console.log('error with updating question', err);
+      }
+    });
+  }
+
+  updateAnswer(e) {
+    this.setState({
+      answerBody: e.target.value
+    });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.updateQuestion}>  
+        <h1 className="headline">Your Answer Here</h1>
+        <textarea value={this.answerBody} placeholder="Answer Question Here" onChange={this.updateAnswer} ></textarea>
+        <button>Submit</button>
+      </form>
+    )
+  }
+}
 
 export default AnswerQuestion;
