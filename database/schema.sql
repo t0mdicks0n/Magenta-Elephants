c6b3de39f258f3a7a8adca8a2960fdaa1e1ae934
oriooctopus <oliverullman@gmail.com>
2017-05-06 13:39:51 -0700

    add database to heroku part1

--- database/schema.sql
+++ database/schema.sql
@@ -2,28 +2,28 @@ DROP DATABASE IF EXISTS `4um`;
 
 CREATE DATABASE 4um;
 
-USE 4um;
+-- USE 4um;
 
-DROP TABLE IF EXISTS `User`;
+-- DROP TABLE IF EXISTS `User`;
 		
-CREATE TABLE `Users` (
-  `id` INTEGER AUTO_INCREMENT,
-  `username` VARCHAR(20) NOT NULL DEFAULT 'NULL',
-  `currentCurrency` INTEGER NULL DEFAULT NULL,
-  `totalCurrency` INTEGER NULL DEFAULT NULL,
-  `noviceRating` INTEGER NULL DEFAULT NULL,
-  `expertRating` INTEGER NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
+-- CREATE TABLE `Users` (
+--   `id` INTEGER AUTO_INCREMENT,
+--   `username` VARCHAR(20) NOT NULL DEFAULT 'NULL',
+--   `currentCurrency` INTEGER NULL DEFAULT NULL,
+--   `totalCurrency` INTEGER NULL DEFAULT NULL,
+--   `noviceRating` INTEGER NULL DEFAULT NULL,
+--   `expertRating` INTEGER NULL DEFAULT NULL,
+--   PRIMARY KEY (`id`)
+-- );
 
-DROP TABLE IF EXISTS `Questions`;
+-- DROP TABLE IF EXISTS `Questions`;
 		
-CREATE TABLE `Questions` (
-  `id` INTEGER AUTO_INCREMENT,
-  `questionTitle` VARCHAR(255) NOT NULL,
-  `questionBody` VARCHAR(500) NULL DEFAULT NULL,
-  `answer` VARCHAR(255) NULL DEFAULT NULL,
-  `Qid_User` INTEGER NOT NULL,
-  `Aid_User` INTEGER NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
+-- CREATE TABLE `Questions` (
+--   `id` INTEGER AUTO_INCREMENT,
+--   `questionTitle` VARCHAR(255) NOT NULL,
+--   `questionBody` VARCHAR(500) NULL DEFAULT NULL,
+--   `answer` VARCHAR(255) NULL DEFAULT NULL,
+--   `Qid_User` INTEGER NOT NULL,
+--   `Aid_User` INTEGER NULL DEFAULT NULL,
+--   PRIMARY KEY (`id`)
+-- );
