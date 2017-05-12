31abc3634a1182899ab6c4f7b1676d6e606de493
oriooctopus <oliverullman@gmail.com>
2017-05-12 15:10:30 -0700

    socket io basics

--- client/socket.js
+++ client/socket.js
@@ -1,1 +1,16 @@
 var socket = io.connect();
+
+socket.on('new message', function(data) {
+  var body = document.getElementById('app')
+  body.append(`<div><strong>${data.user}</strong>${data.msg}</div>`);
+});
+
+var createUser = function(user) {
+  socket.emit('new user', user, () => {});
+};
+
+createUser(String(Math.random()));
+
+setInterval(function() {
+  socket.emit('send message', 'hey');
+}, 2000);
