dc71598d42d475933b8eeb617b026503d490d2e4
Tom Dickson <tom@tomdickson.eu  name = Tom Dickson>
2017-05-17 12:00:54 -0700

    Added ios client react native to project.

---
+++ IOS_client/index.ios.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import {AppRegistry} from 'react-native';
+
+import App from './components/App.js'
+
+AppRegistry.registerComponent('AwesomeProject', () => App);
