b99957c25147b071818295918bdf3050f90a3ffe
Preda Anuvatnujotikul <preda.anu@gmail>
2017-05-17 15:44:23 -0700

    Style included for Question component.

--- IOS_client/components/Question.js
+++ IOS_client/components/Question.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import {
-  View,
-  Image,
-  Text
-} from 'react-native';
+import { View, Image, Text, StyleSheet } from 'react-native';
 import Currency from './Currency.js';
 
 const Question = (props) => {
   if (props.q.avatar) {
     return (
-      <View>
+      <View style={styles.questionBox}>
         <Image 
           source={{uri: props.q.avatar}} 
-          style={{width: 193, height: 110}}/>
-        <Text>{props.q.questionTitle}</Text>
+          style={styles.image}/>
+        <Text style={styles.textArea}>{props.q.questionTitle}</Text>
         <Currency />
       </View>
     )
@@ -26,5 +22,33 @@ const Question = (props) => {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  questionBox: {
+    width: 350,
+    height: 120,
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingRight: 5,
+    paddingLeft: 5,
+    marginBottom: 10,
+    marginRight: 10,
+    backgroundColor: '#C0C0C0',
+    borderColor: 'black',
+    borderWidth: 1,
+    alignItems: 'flex-start',
+    flexDirection: 'row'
+  },
+  image: {
+    width: 80,
+    height: 110
+  },
+  textArea: {
+    paddingLeft: 5,
+    paddingRight: 5,
+    flex: 1,
+    flexWrap: 'wrap'
+  }
+});
 
 export default Question;
