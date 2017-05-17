8a3304f41c6060b25380d838df614e83c6c8a2d6
Preda Anuvatnujotikul <preda.anu@gmail>
2017-05-17 16:29:39 -0700

    Style added for Question component. Created AnsweredStatus component.

--- IOS_client/components/Question.js
+++ IOS_client/components/Question.js
@@ -1,25 +1,37 @@
 import React from 'react';
 import { View, Image, Text, StyleSheet } from 'react-native';
 import Currency from './Currency.js';
+import AnsweredStatus from './AnsweredStatus.js';
 
-const Question = (props) => {
-  if (props.q.avatar) {
-    return (
-      <View style={styles.questionBox}>
-        <Image 
-          source={{uri: props.q.avatar}} 
-          style={styles.image}/>
-        <Text style={styles.textArea}>{props.q.questionTitle}</Text>
-        <Currency />
-      </View>
-    )
-  } else {
-    return (
-      <View>
-        <Text>{props.q.questionTitle}</Text>
-        <Currency />
-      </View>
-    )
+class Question extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      answered: false
+    }
+  }
+
+  render() {
+    if (this.props.q.avatar) {
+      return (
+        <View style={styles.questionBox}>
+          <Image 
+            source={{uri: this.props.q.avatar}} 
+            style={styles.image}/>
+          <Text style={styles.textArea}>{this.props.q.questionTitle}</Text>
+          <Currency />
+          <AnsweredStatus style={styles.answeredStatusBox} answeredStatus={this.state.answered} />
+        </View>
+      )
+    } else {
+      return (
+        <View>
+          <Text>{this.props.q.questionTitle}</Text>
+          <Currency />
+          <AnsweredStatus answeredStatus={this.state.answered} />
+        </View>
+      )
+    }
   }
 }
 
@@ -48,6 +60,11 @@ const styles = StyleSheet.create({
     paddingRight: 5,
     flex: 1,
     flexWrap: 'wrap'
+  },
+  answeredStatusBox: {
+    flex: 1,
+    flexDirection: 'row',
+    bottom: 0,
   }
 });
 
