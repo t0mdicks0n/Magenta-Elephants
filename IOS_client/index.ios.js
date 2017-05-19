a466c3d0164282f91e945953acab042361551ed7
Nick Anderson <nicasioca@gmail.com>
2017-05-19 12:18:27 -0700

    ignore yellow warning box

--- IOS_client/index.ios.js
+++ IOS_client/index.ios.js
@@ -2,4 +2,5 @@ import React from 'react'
 import { AppRegistry } from 'react-native'
 import forumApp from './main'
 
+console.disableYellowBox = true;
 AppRegistry.registerComponent('AwesomeProject', () => forumApp);
